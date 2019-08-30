0fa5202762d6d3b9d532e4f5caf193663d30d8ce
Jofiag <Aguemon.wahab.97@gmail.com>
2019-08-30 10:58:48 +0200

    Modification de la fonction getUser

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.openclassrooms.magicgithub.repository;
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
 
 public class UserRepository {
 
@@ -15,7 +18,9 @@ public class UserRepository {
 
     public List<User> getUsers() {
         // TODO: A modifier
-        return null;
+        List<User> userList = new ArrayList <User>();
+        userList.addAll(FAKE_USERS);
+        return userList;
     }
 
     public void generateRandomUser() {
