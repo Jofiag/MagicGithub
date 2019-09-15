71468c4b4349834d3e8edca5e69a84626763a1e7
Jofiag <Aguemon.wahab.97@gmail.com>
2019-09-15 15:35:17 +0200

    Correction des warnings

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -3,13 +3,7 @@ package com.openclassrooms.magicgithub.repository;
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
-
-import static com.openclassrooms.magicgithub.model.User.*;
 
 public class UserRepository {
 
